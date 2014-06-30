a94326878ab6e538ea16a53b66c85adf186848a1
aransfor <ransfordanthony@gmail.com>
2014-06-30 12:00:34 -0700

    switch config to support 935

--- lib/clients/switchclient/switch_client_config.py
+++ lib/clients/switchclient/switch_client_config.py
@@ -2,7 +2,8 @@ class switch_config(object):
     '''
     configuration file for multiplexer client
     info is the configuration dictionary in the form
-    {channel_name: (port, hint, display_location, stretched)), }
+    {channel_name: (port, display_location, inverted)), }
     '''
     info = {'399': (9, (0,1), False),
+            '935': (10, (0,2), False)
             }
