73be2f43cc0b907a11f6ed3e12ca7b75b3599bad
aransfor <ransfordanthony@gmail.com>
2014-07-17 10:00:36 -0700

    writing one rigol server to rule them all

---
+++ lib/servers/rigolDG1022server/rigolDG1022.py
@@ -0,0 +1,91 @@
+# Copyright (C) 2011 Anthony Ransford
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+"""
+### BEGIN NODE INFO
+[info]
+name = Rigol DG1022A Server
+version = 1.3
+description = 
+
+[startup]
+cmdline = %PYTHON% %FILE%
+timeout = 20
+
+[shutdown]
+message = 987654321
+timeout = 5
+### END NODE INFO
+"""
+
+from labrad.server import setting
+from labrad.gpib import GPIBManagedServer, GPIBDeviceWrapper
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+class RigolWrapper(GPIBDeviceWrapper):
+    
+    def initialize(self):
+        '''
+        Provides a lookup table for waveform to GPIB lingo
+        '''
+        self.lookup = {'sine':'SIN', 'square':'SQU', 'ramp':'RAMP', 'pulse':'PULS', 'noise':'NOIS', 'DC':'DC'}
+    
+    @inlineCallbacks
+    def setOutput(self, chan = None, output):
+        '''
+        Turns on or off the rigol output of specified channel
+        '''
+        if chan == 2:
+            chan = ':CH2'
+        else: chan = ''
+        
+        if output :
+            yield self.device.write("OUTP" + chan + " ON")
+        else:
+            yield self.device.write("OUTP" + chan + " OFF")
+            
+    @inlineCallbacks
+    def applyWaveForm(self, channel, form, frequency, amplitude, offset):
+        '''
+        Applys waveform from self.lookup dictionary with given parameters
+        '''
+        if channel == 2:
+            chan = ':CH2'
+        else: chan = ''
+        
+        output = "APPL:" + self.lookup[form] + chan + ' ' + str(int(frequency)) + ',' + str(amplitude) + ',' + str(offset)
+        yield self.device.write(output)
+
+class RigolServer(GPIBManagedServer):
+    name = 'Rigol DG1022A Server' # Server name
+    deviceName = 'RIGOL TECHNOLOGIES DG1022A' # Model string returned from *IDN?
+    deviceWrapper = RigolWrapper
+
+    @setting(10, 'Set Output', channel = 'i', output = 'b')
+    def setDeviceOutput(self, c, channel = None, output): # uses passed context "c" to address specific device 
+        dev = self.selectedDevice(c)
+        yield dev.setOutput(chan, output)
+    
+    @setting(69, 'Apply Waveform', channel = 'i', form = 's', frequency = ['v[Hz]'], amplitude = ['v[V]'], offset = ['v[V]']  )
+    def applyDeviceWaveform(self, c, channel, form, frequency, amplitude, offset):
+        dev = self.select_device(c)
+        yield dev.applyWaveForm(channel, form, frequency, amplitude, offset)
+
+
+__server__ = RigolServer()
+
+if __name__ == '__main__':
+    from labrad import util
+    util.runServer(__server__)
