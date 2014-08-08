11618c9e632316357cbb16c1706d59d602fbab7c
aransfor <ransfordanthony@gmail.com>
2014-08-07 17:44:30 -0700

    Added optional naming to switch widget on and off

--- lib/clients/qtui/switch.py
+++ lib/clients/qtui/switch.py
@@ -3,8 +3,9 @@ from PyQt4 import QtGui, QtCore
 
 class TextChangingButton(QtGui.QPushButton):
     """Button that changes its text to ON or OFF and colors when it's pressed""" 
-    def __init__(self, parent = None):
+    def __init__(self, labels = None, parent = None):
         super(TextChangingButton, self).__init__(parent)
+        self.labels = labels
         self.setCheckable(True)
         self.setFont(QtGui.QFont('MS Shell Dlg 2',pointSize=10))
         self.setSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
@@ -14,22 +15,28 @@ class TextChangingButton(QtGui.QPushButton):
     
     def setAppearance(self, down):
         if down:
-            self.setText('Closed')
+            if self.labels == None:
+                self.setText('On')
+            else:
+                self.setText(self.labels[0])
             self.setPalette(QtGui.QPalette(QtCore.Qt.darkGreen))
         else:
-            self.setText('Opened')
+            if self.labels == None:
+                self.setText('Off')
+            else:
+                self.setText(self.labels[1])
             self.setPalette(QtGui.QPalette(QtCore.Qt.black))
     
     def sizeHint(self):
         return QtCore.QSize(37, 26)
 
 class QCustomSwitchChannel(QtGui.QFrame):
-    def __init__(self, title, parent=None):
+    def __init__(self, title, labels = None, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.setFrameStyle(0x0001 | 0x0030)
-        self.makeLayout(title)
+        self.makeLayout(title, labels)
     
-    def makeLayout(self, title):
+    def makeLayout(self, title, labels):
         layout = QtGui.QGridLayout()
         title = QtGui.QLabel(title)
         title.setFont(QtGui.QFont('MS Shell Dlg 2',pointSize=16))
@@ -37,13 +44,14 @@ class QCustomSwitchChannel(QtGui.QFrame):
         
         #editable fields
 
-        self.TTLswitch = TextChangingButton()
+        self.TTLswitch = TextChangingButton(labels)
+        self.TTLswitch.setAutoDefault(True)
         layout.addWidget(self.TTLswitch, 1,0, 2,1)          
         self.setLayout(layout)
     
 
 if __name__=="__main__":
     app = QtGui.QApplication(sys.argv)
-    icon = QCustomSwitchChannel('369')
+    icon = QCustomSwitchChannel('369', ('Opened', 'Closed'))
     icon.show()
     app.exec_()
