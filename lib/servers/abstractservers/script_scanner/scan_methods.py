3f7f3f8ba7ad8df32455ac549f347a4be69b39dd
Andrew Jayich <jayich@gmail.com>
2014-10-28 21:27:12 -0700

    Note on trying to avoid importing from scan_methods.py so the file can be fully deprectated.

--- lib/servers/abstractservers/script_scanner/scan_methods.py
+++ lib/servers/abstractservers/script_scanner/scan_methods.py
@@ -1,6 +1,9 @@
 """
 This module is intended to keep old code from breaking that attempts to
 import the classes below from .scan_methods path.
+
+You should avoid importing from scan_methods.py when writing new code, and
+fix old code by not importing from scan_methods.py.
 """
 
 from common.lib.servers.abstractservers.script_scanner.experiment import experiment
