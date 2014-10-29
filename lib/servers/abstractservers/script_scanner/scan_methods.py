96dff4e8ae03823f5cad310d2efab3ea99b826d4
Andrew Jayich <jayich@gmail.com>
2014-10-28 22:09:28 -0700

    Cleaned up import.

--- lib/servers/abstractservers/script_scanner/scan_methods.py
+++ lib/servers/abstractservers/script_scanner/scan_methods.py
@@ -6,8 +6,8 @@ You should avoid importing from scan_methods.py when writing new code, and
 fix old code by not importing from scan_methods.py.
 """
 
-from common.lib.servers.abstractservers.script_scanner.experiment import experiment
-from common.lib.servers.abstractservers.script_scanner.single import single
-from common.lib.servers.abstractservers.script_scanner.repeat_reload import repeat_reload
-from common.lib.servers.abstractservers.script_scanner.scan_experiment_1D import scan_experiment_1D
-from common.lib.servers.abstractservers.script_scanner.scan_experiment_1D_measure import scan_experiment_1D_measure
+from experiment import experiment
+from single import single
+from repeat_reload import repeat_reload
+from scan_experiment_1D import scan_experiment_1D
+from scan_experiment_1D_measure import scan_experiment_1D_measure
