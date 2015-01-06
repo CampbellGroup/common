621da5b436e14cc78be436aa1cafe676360bab73
aransfor <ransfordanthony@gmail.com>
2015-01-05 16:40:33 -0800

    added test experiment

--- lib/config/scriptscanner_config.py
+++ lib/config/scriptscanner_config.py
@@ -2,7 +2,7 @@ class config(object):
 
     #list in the format (import_path, class_name)
     #scripts = [('Qsim.scripts.experiments.tickle.tickle_experiment', 'ticklescan')
-    scripts = [('molecules.lib.exp_control.scripts.scope_periodic', 'ScopeMeasure')
+    scripts = [('common.lib.servers.abstractservers.testexp.testexperiment', 'testexperiment')
                ]
 
     allowed_concurrent = {
