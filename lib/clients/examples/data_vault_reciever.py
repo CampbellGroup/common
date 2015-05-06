209c1422ad8cf804cf27a9a277fd380235b30146
Andrew Jayich <jayich@gmail.com>
2015-05-05 22:46:25 -0700

    simple client for listening to data vault.

---
+++ lib/clients/examples/data_vault_reciever.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Widget to listen for data vault signals
+"""
+from twisted.internet.defer import inlineCallbacks
+from PyQt4 import QtGui
+
+
+class DataVaultReciever(QtGui.QWidget):
+
+    ID = 752311 # ID for the client to register to the server
+
+    def __init__(self, reactor, parent=None):
+        super(DataVaultReciever, self).__init__(parent)
+        self.reactor = reactor
+        self.setupLayout()
+        self.connect()
+
+    def setupLayout(self):
+        #setup the layout and make all the widgets
+        self.setWindowTitle('Data Vault Reciever')
+        #create a horizontal layout
+        layout = QtGui.QHBoxLayout()
+        #create the text widget 
+        self.textedit = QtGui.QTextEdit()
+        self.textedit.setReadOnly(True)
+        layout.addWidget(self.textedit)
+        self.setLayout(layout)
+
+    @inlineCallbacks
+    def connect(self):
+        from labrad.wrappers import connectAsync
+        cxn = yield connectAsync(name = 'Signal Widget')
+        self.dv = cxn.data_vault
+        yield self.dv.signal__data_available(self.ID)
+        # connect to signal from server (note the method is named from parsed 
+        # text of the in the server emitter name)
+        yield self.dv.addListener(listener=self.display_signal, 
+                                      source=None, ID=self.ID) 
+        # This registers the client as a listener to the server and assigns a 
+        # slot (function) from the client to the signal emitted from the server
+        # In this case self.displaySignal
+
+    def display_signal(self, cntx, signal):
+        self.textedit.append(signal)
+
+    def closeEvent(self, x):
+        #stop the reactor when closing the widget
+        self.reactor.stop()
+
+
+if __name__=="__main__":
+    #join Qt and twisted event loops
+    a = QtGui.QApplication( [] )
+    import common.lib.qt4reactor as qt4reactor
+    qt4reactor.install()
+    from twisted.internet import reactor
+    widget = DataVaultReciever(reactor)
+    widget.show()
+    reactor.run()
