9febdf84efc3c8bda66e3601f8638ae9c03b11e9
jayich <jayich@users.noreply.github.com>
2015-05-08 12:30:21 -0700

    Update multiplexerclient_config.py

--- lib/config/multiplexerclient_config.py
+++ lib/config/multiplexerclient_config.py
@@ -2,7 +2,7 @@ class multiplexer_config(object):
     '''
     configuration file for multiplexer client
     info is the configuration dictionary in the form
-    {channel_name: (port, hint, display_location, stretched)), }
+    {channel_name: (port, hint, display_location, stretched, display_pid)), }
     '''
     info = {'Channel 1' :(1, '384.230000', (1,1), True, False),
             'Channel 2' :(2, '384.230000', (1,2), True, False),
