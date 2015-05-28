c45318ad96f7a51d5210cb43386ff1002d34b234
aransfor <ransfordanthony@gmail.com>
2015-05-28 10:38:20 -0700

    working on PID stuff for wavemeter

--- lib/servers/Mulitplexer/Multiplexerserver.py
+++ lib/servers/Mulitplexer/Multiplexerserver.py
@@ -30,6 +30,83 @@ LOCKSIGNAL = 112456
 OUTPUTCHANGED = 121212
 PIDVOLTAGE = 902484
 
+class PID(object):
+    """
+    Discrete PID control
+    """
+
+    def __init__(self, P=2.0, I=0.0, D=1.0, Derivator=0, Integrator=0, Integrator_max=500, Integrator_min=-500):
+
+        self.Kp=P
+        self.Ki=I
+        self.Kd=D
+        self.Derivator=Derivator
+        self.Integrator=Integrator
+        self.Integrator_max=Integrator_max
+        self.Integrator_min=Integrator_min
+
+        self.set_point=0.0
+        self.error=0.0
+
+    def update(self,current_value):
+        """
+        Calculate PID output value for given reference input and feedback
+        """
+
+        self.error = self.set_point - current_value
+
+        self.P_value = self.Kp * self.error
+        self.D_value = self.Kd * ( self.error - self.Derivator)
+        self.Derivator = self.error
+
+        self.Integrator = self.Integrator + self.error
+
+        if self.Integrator > self.Integrator_max:
+            self.Integrator = self.Integrator_max
+        elif self.Integrator < self.Integrator_min:
+            self.Integrator = self.Integrator_min
+
+        self.I_value = self.Integrator * self.Ki
+
+        PID = self.P_value + self.I_value + self.D_value
+
+        return PID
+
+    def setPoint(self,set_point):
+        """
+        Initilize the setpoint of PID
+        """
+        self.set_point = set_point
+        self.Integrator=0
+        self.Derivator=0
+
+    def setIntegrator(self, Integrator):
+        self.Integrator = Integrator
+
+    def setDerivator(self, Derivator):
+        self.Derivator = Derivator
+
+    def setKp(self,P):
+        self.Kp=P
+
+    def setKi(self,I):
+        self.Ki=I
+
+    def setKd(self,D):
+        self.Kd=D
+
+    def getPoint(self):
+        return self.set_point
+
+    def getError(self):
+        return self.error
+
+    def getIntegrator(self):
+        return self.Integrator
+
+    def getDerivator(self):
+        return self.Derivator
+
 class MultiplexerServer(LabradServer):
     """
     Multiplexer Server for Wavelength Meter
@@ -193,8 +270,6 @@ class MultiplexerServer(LabradServer):
         volts = yield self.wmdll.GetDeviationSignalNum(chan_c,self.d)
         self.pidvoltagechanged((chan,volts))
         returnValue(volts)  
-        
-
     
     @setting(26, "Get Switcher Signal State", chan = 'i', returns = 'b')
     def getSwitcherState(self, c, chan):
@@ -221,15 +296,22 @@ class MultiplexerServer(LabradServer):
         else:
             value = False
         returnValue(value)
-
-            
+    
+    @setting(29, "Get Individual Lock State", returns = 'b')
+    def getSingleLockState(self,c,chan):
+        return False
+        
     def measureChan(self):
         #TODO: Improve this with a looping call
         reactor.callLater(0.1, self.measureChan)
         for chan in range(8):
             if self.getSwitcherState(self, chan + 1):
                 self.getFrequency(self, chan + 1) 
-                self.getOutputVoltage(self, chan + 1)    
+                self.getOutputVoltage(self, chan + 1)
+                if self.getSingleLockState(self, chan + 1):
+                    value = self.calcPID(chan)
+                    print value
+                    #self.setDACVoltage(chan + 1, value )
         
 if __name__ == "__main__":
     from labrad import util
