efe0d5f95ae42dbe9ef90ef8205cde80927c5717
Andrew Jayich <jayich@gmail.com>
2015-06-15 17:07:17 -0700

    API changes for setting and getting the voltage.  Cleaned up server name and class name.
    Round output of LabJack to 4 digits of precision (overkill)
    
    Input and output now use WithUnit types.

--- lib/servers/Labjack/Labjackserver.py
+++ lib/servers/Labjack/Labjackserver.py
@@ -1,16 +1,15 @@
 from labrad.server import LabradServer, setting
-from twisted.internet.defer import returnValue, inlineCallbacks
-from twisted.internet import reactor
-from labrad import types as T
 from twisted.internet.defer import returnValue
+from labrad.units import WithUnit
 from Labjackapi import u3
+
 
 """
 ### BEGIN NODE INFO
 [info]
 name = LabJack
 version = 1.0
-description = 
+description =
 instancename = LabJack
 
 [startup]
@@ -24,36 +23,57 @@ timeout = 20
 """
 
 
-'''
-Created on Dec 19, 2014
-
-@author: anthonyransford
-
-'''
-
-class LabJackserver(LabradServer):
+class LabJackServer(LabradServer):
     """
     LabJack DAC and ADC server
     """
-    name = 'LabJackserver'
-    
+    name = 'LabJack'
+
     def initServer(self):
         try:
             self.device = u3.U3()
         except:
             print "ERROR: Could not connect to LabJack"
-        
-    @setting(11, 'Set DAC', chan = 'i',  volts = 'v')
-    def dacOutput(self, c, chan, volts):
-        dac_value = self.device.voltageToDACBits(volts, dacNumber = chan, is16Bits = True)
-        self.device.getFeedback(u3.DAC16(chan, dac_value))
-        
-    @setting(12, 'Get ADC Voltage', chan = 'i', returns = 'v')
-    def Ain(self, c, chan):
-        ainbits, = self.device.getFeedback(u3.AIN(chan))
-        ainValue = yield self.device.binaryToCalibratedAnalogVoltage(ainbits, isLowVoltage = False, channelNumber = chan)
-        returnValue(ainValue)
-        
+
+    @setting(11, channel='i',  volts='v')
+    def output_voltage(self, c, channel, volts):
+        """
+        Set the analog output voltage of channel.
+
+        Parameters
+        ----------
+        channel: int, channel to ouput DC voltage on.
+        volts: WithUnit, voltage to output
+        """
+        # convert value to volts for LabJack API
+        volts_without_units = volts['V']
+        dac_value = self.device.voltageToDACBits(volts_without_units, dacNumber = channel, is16Bits = True)
+        self.device.getFeedback(u3.DAC16(channel, dac_value))
+
+    @setting(12, channel='i', returns='v')
+    def measure_voltage(self, c, channel):
+        """
+        Measure the analog input voltage at channel.
+
+        The value is rounded to 4 digits of precision.  This is probably far
+        beyond the LabJack's capabilities.
+
+        Parameters
+        ----------
+        channel: int, channel to ouput DC voltage on.
+
+        Returns
+        -------
+        WithUnit voltage value
+        """
+        ainbits, = self.device.getFeedback(u3.AIN(channel))
+        value = yield self.device.binaryToCalibratedAnalogVoltage(ainbits, isLowVoltage = False, channelNumber = channel)
+        value = round(value, 4)
+        # convert value to WithUnit, assumes value is in Volts.
+        voltage = WithUnit(value, 'V')
+        returnValue(voltage)
+
+
 if __name__ == "__main__":
     from labrad import util
-    util.runServer( LabJackserver() )
+    util.runServer(LabJackServer())
