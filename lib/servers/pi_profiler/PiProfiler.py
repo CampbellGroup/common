460548253bfd957682bf9eed59d05d34fd2740bf
Andrew Jayich <jayich@gmail.com>
2015-11-15 15:38:31 -0800

    Basic camera functionality now seems to work.  The server provides access to R, G, and B separately.

--- lib/servers/pi_profiler/PiProfiler.py
+++ lib/servers/pi_profiler/PiProfiler.py
@@ -4,7 +4,7 @@
 [info]
 name = Pi Camera
 version = 1.0
-description = 
+description =
 instancename = PiCamera
 
 [startup]
@@ -26,78 +26,67 @@ from twisted.internet.defer import returnValue
 class PiCamera(LabradServer):
     """
     Raspberry Pi based camera server.
-    
-    This can take basic images, and later it should be able to get beam 
+
+    This can take basic images, and later it should be able to get beam
     profiles.
     """
     name = 'Pi Camera'
     _image_data = None
 
-
     @setting(1)
     def capture_image(self, c):
         """
-        Take a picture with the camera, populating self._image_data with a 
+        Take a picture with the camera, populating self._image_data with a
         numpy array.
         """
         with picamera.PiCamera() as camera:
             with picamera.array.PiRGBArray(camera) as output:
                 camera.capture(output, 'rgb')
                 print('Captured %dx%d image' % (
-                        output.array.shape[1], output.array.shape[0]))
+                      output.array.shape[1], output.array.shape[0]))
                 self._image_data = output.array
-
-        print "capture_image type(self._image_data):", type(self._image_data)
-
 
     @setting(2)
     def image_array(self, returns='?'):
         """
-        Return the numpy array of capture image data after it has been 
-        flattened to be something like greyscale, summing over RGB elements.
+        Return the numpy array of capture image data.
         """
+        image_array = self._image_data
+        image_array = _n.array(image_array, dtype='float')
         yield None
-        print "image_array type(self._image_data):", type(self._image_data)
-        print "self._image_data[0]:", self._image_data[0]
-        print "len(self._image_data):", len(self._image_data)
-        print "len(self._image_data[0]):", len(self._image_data[0])
-        print "len(self._image_data[0][0]):", len(self._image_data[0][0])
-        print "self._image_data[0][0]:", self._image_data[0][0]
-        print "sum(self._image_data[0][0]):", sum(self._image_data[0][0])
-        returnValue(self._image_data)
+        returnValue(image_array)
 
     @setting(3)
     def red_image_array(self, returns='?'):
         """
         Return the numpy array of R values of captured image data.
-        """        
+        """
         red_array = self._image_data[:, :, 0]
         red_array = _n.array(red_array, dtype='float')
-        print "type(red_array):", type(red_array)
-        print "red_array[0]:", red_array[0]
         yield None
-        returnValue(red_array)        
-
+        returnValue(red_array)
 
     @setting(4)
     def green_image_array(self, returns='?'):
         """
         Return the numpy array of G values of captured image data.
-        """        
+        """
         green_array = self._image_data[:, :, 1]
+        green_array = _n.array(green_array, dtype='float')
         yield None
-        returnValue(green_array) 
+        returnValue(green_array)
 
     @setting(5)
     def blue_image_array(self, returns='?'):
         """
         Return the numpy array of B values of captured image data.
-        """        
+        """
         blue_array = self._image_data[:, :, 2]
+        blue_array = _n.array(blue_array, dtype='float')
         yield None
-        returnValue(blue_array)   
-        
+        returnValue(blue_array)
+
+
 if __name__ == "__main__":
     from labrad import util
     util.runServer(PiCamera())
-    
