116bad787a372e91fdb910261d76799537bfb3f7
Andrew Jayich <jayich@gmail.com>
2015-12-28 17:59:27 -0800

    Mocks for the arduinottl server and for the dual_pmtflow server.

---
+++ tests/mocks/lib/servers/shutterandswitch/mock_Arduinoshutterandswitchserver.py
@@ -0,0 +1,13 @@
+"""
+Mock the arduino TTL controller.
+"""
+
+
+class Mock_ArduinoTTL(object):
+    """
+    """
+
+    name = 'ArduinoTTL'
+
+    def ttl_output(self, chan=None, state=None):
+        pass
