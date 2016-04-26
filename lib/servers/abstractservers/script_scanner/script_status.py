c3cae84a5fe8f2fbf04fa717bf134e8dcda23ac8
Andrew Jayich <jayich@gmail.com>
2016-04-25 18:46:13 -0700

    pep8.

--- lib/servers/abstractservers/script_scanner/script_status.py
+++ lib/servers/abstractservers/script_scanner/script_status.py
@@ -18,13 +18,16 @@ class script_semaphore(object):
         return (self.status, self.percentage_complete)
 
     def set_percentage(self, perc):
-        if not 0.0 <= perc <= 100.0: raise Exception ("Incorrect Percentage of Completion")
+        if not 0.0 <= perc <= 100.0:
+            raise Exception("Incorrect Percentage of Completion")
         self.percentage_complete = perc
-        self.signals.on_running_new_status((self.ident, self.status, self.percentage_complete))
+        self.signals.on_running_new_status((self.ident, self.status,
+                                            self.percentage_complete))
 
     def launch_confirmed(self):
         self.status = 'Running'
-        self.signals.on_running_new_status((self.ident, self.status, self.percentage_complete))
+        self.signals.on_running_new_status((self.ident, self.status,
+                                            self.percentage_complete))
 
     @inlineCallbacks
     def pause(self):
@@ -33,9 +36,11 @@ class script_semaphore(object):
         '''
         if self.pause_lock.locked:
             self.status = 'Paused'
-            self.signals.on_running_new_status((self.ident, self.status, self.percentage_complete))
+            self.signals.on_running_new_status((self.ident, self.status,
+                                                self.percentage_complete))
+
             self.signals.on_running_script_paused((self.ident, True))
-            #call back all pause requests
+            # call back all pause requests
             while self.pause_requests:
                 request = self.pause_requests.pop()
 #                print 'called back pause requests', request
@@ -46,9 +51,11 @@ class script_semaphore(object):
 #        print 'script proceeding'
         if self.status == 'Paused':
             self.status = 'Running'
-            self.signals.on_running_new_status((self.ident, self.status, self.percentage_complete))
+            self.signals.on_running_new_status((self.ident, self.status,
+                                                self.percentage_complete))
+
             self.signals.on_running_script_paused((self.ident, False))
-            #call back all continue requests
+            # call back all continue requests
             while self.continue_requests:
                 request = self.continue_requests.pop()
                 request.callback(True)
@@ -56,28 +63,34 @@ class script_semaphore(object):
     def set_stopping(self):
         self.should_stop = True
         self.status = 'Stopping'
-        self.signals.on_running_new_status((self.ident, self.status, self.percentage_complete))
-        #if was paused, unpause:
+        self.signals.on_running_new_status((self.ident, self.status,
+                                            self.percentage_complete))
+
+        # if was paused, unpause:
         if self.pause_lock.locked:
             self.pause_lock.release()
 
     def set_pausing(self, should_pause):
-        '''if asking to pause, returns a deferred which is fired when sciprt actually paused'''
+        '''if asking to pause, returns a deferred which is fired when sciprt
+        actually paused'''
         if should_pause:
             request = Deferred()
             print 'made request', request
             self.pause_requests.append(request)
             if not self.pause_lock.locked:
-                #if not already paused
-                self.pause_lock.acquire()#immediately returns a deferred that we don't use
+                # if not already paused
+                # immediately returns a deferred that we don't use
+                self.pause_lock.acquire()
                 self.status = 'Pausing'
-                self.signals.on_running_new_status((self.ident, self.status, self.percentage_complete))
+                self.signals.on_running_new_status((self.ident, self.status,
+                                                    self.percentage_complete))
+
 #                print 'acquired pause', request
             else:
                 print 'not acquiring because locked'
         else:
             if not self.pause_lock.locked:
-                raise Exception ("Trying to unpause script that was not paused")
+                raise Exception("Trying to unpause script that was not paused")
             request = Deferred()
             self.continue_requests.append(request)
 #            print 'releasing the lock!'
@@ -87,11 +100,13 @@ class script_semaphore(object):
     def stop_confirmed(self):
         self.should_stop = False
         self.status = 'Stopped'
-        self.signals.on_running_new_status((self.ident, self.status, self.percentage_complete))
+        self.signals.on_running_new_status((self.ident, self.status,
+                                            self.percentage_complete))
+
         self.signals.on_running_script_stopped(self.ident)
 
     def finish_confirmed(self):
-        #call back all pause requests
+        # call back all pause requests
         for request_list in [self.pause_requests, self.continue_requests]:
             while(request_list):
                 request = request_list.pop()
@@ -99,10 +114,14 @@ class script_semaphore(object):
         if not self.status == 'Stopped':
             self.percentage_complete = 100.0
             self.status = 'Finished'
-            self.signals.on_running_new_status((self.ident, self.status, self.percentage_complete))
+            self.signals.on_running_new_status((self.ident, self.status,
+                                                self.percentage_complete))
+
         self.signals.on_running_script_finished(self.ident)
 
     def error_finish_confirmed(self, error):
         self.status = 'Error'
-        self.signals.on_running_new_status((self.ident, self.status, self.percentage_complete))
+        self.signals.on_running_new_status((self.ident, self.status,
+                                            self.percentage_complete))
+
         self.signals.on_running_script_finished_error((self.ident, error))
