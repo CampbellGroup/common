8153d3860349c68796e4aeb9ae9839e0c938f1e8
Xueping Long <gregllong@gmail.com>
2016-05-07 16:12:18 -0700

    TextChangingButton for the rigol clients
    Signed-off-by: Xueping Long <gregllong@gmail.com>

--- lib/clients/rigolcontroller/rigolcontroller.py
+++ lib/clients/rigolcontroller/rigolcontroller.py
@@ -1,35 +1,9 @@
 from common.lib.clients.qtui.QCustomSpinBox import QCustomSpinBox
 from twisted.internet.defer import inlineCallbacks
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui
+from common.lib.clients.qtui.q_custom_text_changing_button import \
+    TextChangingButton
 
-class TextChangingButton(QtGui.QPushButton):
-    """Button that changes its text to ON or OFF and colors when it's pressed"""
-    def __init__(self, labels = None, parent = None):
-        super(TextChangingButton, self).__init__(parent)
-        self.labels = labels
-        self.setCheckable(True)
-        self.setFont(QtGui.QFont('MS Shell Dlg 2',pointSize=10))
-        self.setSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
-        #connect signal for appearance changing
-        self.toggled.connect(self.setAppearance)
-        self.setAppearance(self.isDown())
-
-    def setAppearance(self, down):
-        if down:
-            if self.labels == None:
-                self.setText('On')
-            else:
-                self.setText(self.labels[0])
-            self.setPalette(QtGui.QPalette(QtCore.Qt.darkGreen))
-        else:
-            if self.labels == None:
-                self.setText('Off')
-            else:
-                self.setText(self.labels[1])
-            self.setPalette(QtGui.QPalette(QtCore.Qt.black))
-
-    def sizeHint(self):
-        return QtCore.QSize(37, 26)
 
 class rigolclient(QtGui.QWidget):
 
