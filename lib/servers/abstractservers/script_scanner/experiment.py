c0abe8fbfabd4fff5dfaa1c988e1cdc288f53094
Andrew Jayich <jayich@gmail.com>
2016-05-19 01:50:26 -0700

    pep8.

--- lib/servers/abstractservers/script_scanner/experiment.py
+++ lib/servers/abstractservers/script_scanner/experiment.py
@@ -3,9 +3,11 @@ import labrad
 from treedict import TreeDict
 from common.lib.servers.abstractservers.script_scanner.experiment_info import experiment_info
 
+
 class experiment(experiment_info):
 
-    def __init__(self, name = None, required_parameters = None, cxn = None, min_progress = 0.0, max_progress = 100.0,):
+    def __init__(self, name=None, required_parameters=None, cxn=None,
+                 min_progress=0.0, max_progress=100.0,):
         required_parameters = self.all_required_parameters()
         super(experiment, self).__init__(name, required_parameters)
         self.cxn = cxn
@@ -68,27 +70,28 @@ class experiment(experiment_info):
     def _run(self, cxn, context):
         self.run(cxn, context)
 
-    def _load_required_parameters(self, overwrite = False):
+    def _load_required_parameters(self, overwrite=False):
         d = self._load_parameters_dict(self.required_parameters)
-        self.parameters.update(d, overwrite = overwrite)
+        self.parameters.update(d, overwrite=overwrite)
 
     def _load_parameters_dict(self, params):
         '''loads the required parameter into a treedict'''
         d = TreeDict()
-        for collection,parameter_name in params:
+        for collection, parameter_name in params:
             try:
                 value = self.pv.get_parameter(collection, parameter_name)
             except Exception as e:
                 print e
-                raise Exception ("In {}: Parameter {} not found among Parameter Vault parameters".format(self.name, (collection, parameter_name)))
+                message = "In {}: Parameter {} not found among Parameter Vault parameters"
+                raise Exception (message.format(self.name, (collection, parameter_name)))
             else:
                 d['{0}.{1}'.format(collection, parameter_name)] = value
-                #print d.items()
         return d
 
     def set_parameters(self, parameter_dict):
         '''
-        can reload all parameter values from parameter_vault or replace parameters with values from the provided parameter_dict
+        can reload all parameter values from parameter_vault or replace
+        parameters with values from the provided parameter_dict
         '''
         if isinstance(parameter_dict, dict):
             udpate_dict = TreeDict()
@@ -97,7 +100,8 @@ class experiment(experiment_info):
         elif isinstance(parameter_dict, TreeDict):
             udpate_dict = parameter_dict
         else:
-            raise Exception ("Incorrect input type for the replacement dictionary")
+            message = "Incorrect input type for the replacement dictionary"
+            raise Exception(message)
         self.parameters.update(udpate_dict)
 
     def reload_some_parameters(self, params):
@@ -105,13 +109,13 @@ class experiment(experiment_info):
         self.parameters.update(d)
 
     def reload_all_parameters(self):
-        self._load_required_parameters(overwrite = True)
+        self._load_required_parameters(overwrite=True)
 
     def _finalize(self, cxn, context):
         self.finalize(cxn, context)
         self.sc.finish_confirmed(self.ident)
 
-    #useful functions to be used in subclasses
+    # useful functions to be used in subclasses
     @classmethod
     def all_required_parameters(cls):
         return []
@@ -126,7 +130,7 @@ class experiment(experiment_info):
         return self.should_stop
 
     def make_experiment(self, subexprt_cls):
-        subexprt = subexprt_cls(cxn = self.cxn)
+        subexprt = subexprt_cls(cxn=self.cxn)
         subexprt._connect()
         subexprt._load_required_parameters()
         return subexprt
@@ -134,15 +138,18 @@ class experiment(experiment_info):
     def set_progress_limits(self, min_progress, max_progress):
         self.min_progress = min_progress
         self.max_progress = max_progress
-    #functions to reimplement in the subclass
+
+    # functions to reimplement in the subclass
     def initialize(self, cxn, context, ident):
         '''
         implemented by the subclass
         '''
-    def run(self, cxn, context, replacement_parameters = {}):
+
+    def run(self, cxn, context, replacement_parameters={}):
         '''
         implemented by the subclass
         '''
+
     def finalize(self, cxn, context):
         '''
         implemented by the subclass
