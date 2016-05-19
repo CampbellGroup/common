2138f6b353b08414eafd50607adb5131b4ff75a2
Andrew Jayich <jayich@gmail.com>
2016-05-19 01:33:38 -0700

    pep8 and doctoring improvements.

--- lib/servers/abstractservers/script_scanner/experiment_info.py
+++ lib/servers/abstractservers/script_scanner/experiment_info.py
@@ -1,13 +1,20 @@
 from treedict import TreeDict
 
+
 class experiment_info(object):
     '''
     holds informaton about the experiment
+
+    Attributes
+    ----------
+    name: str
+    parameters: TreeDict
+    required_parameters: list
     '''
     required_parameters = []
     name = ''
-    
-    def __init__(self, name = None, required_parameters = None):
+
+    def __init__(self, name=None, required_parameters=None):
         if name is not None:
             self.name = name
         if required_parameters is not None:
