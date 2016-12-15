3dd9d6b106eb3a0bcf2c94f840bfbfa5b8d4051c
aransfor <ransfordanthony@gmail.com>
2016-12-15 07:01:59 -0800

    scan request returns unitless now

--- lib/clients/script_scanner_gui/tree_view/Data.py
+++ lib/clients/script_scanner_gui/tree_view/Data.py
@@ -276,31 +276,46 @@ class StringNode(Node):
             self._value = value
 
 class ScanNode(Node):
-    
+
     columns = 8
-    
+
     def __init__(self, name, info, parent=None):
         super(ScanNode, self).__init__(name, parent)
         self._collection = parent.name()
         self.set_full_info(info)
-    
+
     def set_full_info(self, info):
         limit_info, scan_info = info
-        self._units = limit_info[0].units
-        self._min = limit_info[0][self._units]
-        self._max = limit_info[1][self._units]
-        self._scan_start = scan_info[0][self._units]
-        self._scan_stop = scan_info[1][self._units]
+        try:
+            self._units = limit_info[0].units
+        except:
+            self._units = None
+        if self._units:
+            self._min = limit_info[0][self._units]
+            self._max = limit_info[1][self._units]
+            self._scan_start = scan_info[0][self._units]
+            self._scan_stop = scan_info[1][self._units]
+        else:
+            self._min = limit_info[0]
+            self._max = limit_info[1]
+            self._scan_start = scan_info[0]
+            self._scan_stop = scan_info[1]
         self._scan_points = scan_info[2]
-    
+
     def path(self):
         return (self._collection, self.name())
-    
+
     def full_parameter(self):
         WithUnit = self.WithUnit
-        return ('scan', ([WithUnit(self._min, self._units), WithUnit(self._max, self._units) ],
-                         (WithUnit(self._scan_start, self._units), WithUnit(self._scan_stop, self._units), self._scan_points)
-                         ))    
+        if self._units:
+            return ('scan', ([WithUnit(self._min, self._units), WithUnit(self._max, self._units)],
+                    (WithUnit(self._scan_start, self._units), WithUnit(self._scan_stop, self._units), self._scan_points)
+                    ))
+        else:
+            return ('scan', ([self._min, self._max],
+                    (self._scan_start, self._scan_stop, self._scan_points)
+                         ))
+
     def data(self, column):
         if column < 1:
             return super(ScanNode, self).data(column)
