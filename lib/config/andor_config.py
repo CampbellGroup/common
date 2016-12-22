8c479a55f4b94828e3571b359cdc440f68beb193
Andrew Jayich <jayich@gmail.com>
2016-12-22 15:12:33 -0800

    Qsim andor config.
    Included a module docstring from the previous config.
    
    pep8.

--- lib/config/andor_config.py
+++ lib/config/andor_config.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
-"""
-Andor iXon configuration file.
-"""
+"""Andor iXon configuration file."""
 
 
-class andor_configuration(object):
-    """path to atmcd32d.dll SDK library"""
-    path_to_dll = 'C:\\Program Files\\Andor SOLIS\\Drivers\\atmcd32d.dll'
-    set_temperature = -20  # degrees C
+class andor_config(object):
+    '''
+    path to atmcd32d.dll SDK library
+    '''
+    path_to_dll = 'C:\\Program Files\\Andor SOLIS\\atmcd32d.dll'
+    # default parameters
+    set_temperature = -80 # degrees C
     read_mode = 'Image'
     acquisition_mode = 'Single Scan'
     trigger_mode = 'Internal'
-    exposure_time = 0.100  # seconds
-    binning = [1, 1]  # numbers of pixels for horizontal and vertical binning
+    exposure_time = 0.12 # seconds
+    binning = [1, 1] # numbers of pixels for horizontal and vertical binning
+    image_path = 'C:\\Users\\QSimYb\\Desktop\\camera_images\\'
