598733c4a3497bf3a28439402ae334f5b599cec6
Xueping Long <gregllong@gmail.com>
2017-01-17 17:48:04 -0800

    a working version of timed voltmeter where whether data should be taken can be set by a serial input.

--- lib/servers/ArduinoVoltmeter/timed_voltmeter/timed_voltmeter.ino
+++ lib/servers/ArduinoVoltmeter/timed_voltmeter/timed_voltmeter.ino
@@ -1,25 +1,50 @@
-unsigned long time;
-long gatetime = 1000
+unsigned long time_in_us;
+unsigned long time_in_ms;
+unsigned long output;
+int sensorValue;
+float v;
+int voltage;
+unsigned long gatetime = 1000; // default gate time in us
+boolean readVoltage = false;
+// Arduino has a time counting limit beyond which the micro() value gets reset. 
+// The following variable tries to avoid this possibility.
+// Can be changed to suit your needs.
+int upper_time_limit = 10000; // time in ms
   
-  void setup() {
-  // initialize serial communication at 115200 bauds per second:
-  Serial.begin(115200);
-
+void setup() {
+  // initialize serial communication at 9600 bauds per second. This is the 
+  // fastest I can do without receiving weird stuff from the serial port;
+  Serial.begin(9600);
 }
 
 void loop() {
-  //using a while loop to reduce the overhead time
-  while (true)
-  {
-    Serial.print("Time: ");
-    time = micros();
-    //prints time since program started
-    Serial.println(time);
-    int sensorValue = analogRead(A0);
-    float voltage = sensorValue * (5.0 / 1023.0);
-    Serial.print("V: ");
-    Serial.println(voltage);
-    //wait 1 ms and take the next data
-    delayMicroseconds(1000);
+  if (Serial.available()) {
+    readVoltage = Serial.read();
+  }
+  // use a while loop to reduce the overhead time;
+  while (readVoltage) {
+    // get time since program started;
+    time_in_us = micros();
+    time_in_ms = (unsigned long) time_in_us/1000;
+    // avoid timing confusion because of Arduino's timer reset;
+    while (time_in_ms > upper_time_limit) {
+    time_in_ms = time_in_ms - upper_time_limit;
+    }
+    // get voltage in mV
+    sensorValue = analogRead(A0);
+    v = sensorValue * (5.0 / 1024.0) * 1000;
+    voltage = (int) v;
+    // combine the time information to voltage information. Scaling factor of 10000
+    // comes from the fact that voltage never exceeds 5000 mV (or 5V).
+    output = time_in_ms * 10000 + voltage;
+    Serial.println(output);
+    //wait gatetime to take the next data
+    delayMicroseconds(gatetime);
+    if (Serial.available()) {
+      readVoltage = Serial.read();
+      if (readVoltage=0) {
+        break;
+      }
+    }
   }
 }
