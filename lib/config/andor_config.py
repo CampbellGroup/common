91520386b63b9b0eeb43bb1388f55702b178af02
NAPOLEON\scientist <mingyufan212@gmail.com>
2017-08-04 11:35:25 -0700

    Add example andor_config.py to common/lib/configs, and delete the .gitignore in the iXonServer folder.

---
+++ lib/config/andor_config.py
@@ -0,0 +1,13 @@
+class AndorConfig(object):
+    '''
+    path to atmcd64d.dll SDK library
+    '''
+    #default parameters
+    path_to_dll = ('C:\\Program Files\\Andor SOLIS\\atmcd64d_legacy.dll')
+    set_temperature = -20 #degrees C
+    read_mode = 'Image'
+    acquisition_mode = 'Single Scan'
+    trigger_mode = 'Internal'
+    exposure_time = 0.100 #seconds
+    binning = [1, 1] #numbers of pixels for horizontal and vertical binning
+    image_path = ('C:\\Users\\scientist\\Pictures\\iXonImages\\')
