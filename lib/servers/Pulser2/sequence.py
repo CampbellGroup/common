dcaa04bd31b053b25e4ef2b5db9a22ff0bc32e27
theoriginaljuice <physics.justin.christensen@gmail.com>
2018-04-07 15:25:13 -0700

    Fixed double switching error for turning on the same TTL channel consecutively. Tested

--- lib/servers/Pulser2/sequence.py
+++ lib/servers/Pulser2/sequence.py
@@ -19,25 +19,25 @@ class Sequence():
         #dictionary in the form time:which channels to switch
         #time is expressed as timestep with the given resolution
         #which channels to switch is a channelTotal-long array with 1 to switch ON, -1 to switch OFF, 0 to do nothing
-        self.switchingTimes = {0:numpy.zeros(self.channelTotal, dtype = numpy.int8)} 
+        self.switchingTimes = {0:numpy.zeros(self.channelTotal, dtype = numpy.int8)}
         self.switches = 1 #keeps track of how many switches are to be performed (same as the number of keys in the switching Times dictionary"
         #dictionary for storing information about dds switches, in the format:
         #timestep: {channel_name: integer representing the state}
         self.ddsSettingList = []
         self.advanceDDS = hardwareConfiguration.channelDict['AdvanceDDS'].channelnumber
         self.resetDDS = hardwareConfiguration.channelDict['ResetDDS'].channelnumber
-    
+
     def addDDS(self, name, start, num, typ):
         timeStep = self.secToStep(start)
         self.ddsSettingList.append((name, timeStep, num, typ))
-            
+
     def addPulse(self, channel, start, duration):
         """adding TTL pulse, times are in seconds"""
         start = self.secToStep(start)
         duration = self.secToStep(duration)
         self._addNewSwitch(start, channel, 1)
         self._addNewSwitch(start + duration, channel, -1)
-    
+
     def extendSequenceLength(self, timeLength):
         """Allows to extend the total length of the sequence"""
         timeLength = self.secToStep(timeLength)
@@ -46,11 +46,11 @@ class Sequence():
     def secToStep(self, sec):
         '''converts seconds to time steps'''
         start = '{0:.9f}'.format(sec) #round to nanoseconds
-        start = Decimal(start) #convert to decimal 
+        start = Decimal(start) #convert to decimal
         step = ( start / self.timeResolution).to_integral_value()
         step = int(step)
         return step
-    
+
 #     def numToHex(self, number):
 #         '''converts the number to the hex representation for a total of 32 bits
 #         i.e: 3 -> 00000000...000100 ->  \x00\x00\x03\x00, note that the order of 8bit pieces is switched'''
@@ -69,27 +69,33 @@ class Sequence():
 
     def _addNewSwitch(self, timeStep, chan, value):
         if self.switchingTimes.has_key(timeStep):
-            if self.switchingTimes[timeStep][chan]: raise Exception ('Double switch at time {} for channel {}'.format(timeStep, chan))
-            self.switchingTimes[timeStep][chan] = value
+            if self.switchingTimes[timeStep][chan]: # checks if 0 or 1/-1
+                # if set to turn off, but want on, replace with zero, fixes error adding 2 TTLs back to back
+                if self.switchingTimes[timeStep][chan] == -1 and value == 1:
+                    self.switchingTimes[timeStep][chan] = 0
+                else:
+                    raise Exception ('Double switch at time {} for channel {}'.format(timeStep, chan))
+            else:
+                self.switchingTimes[timeStep][chan] = value
         else:
             if self.switches == self.MAX_SWITCHES: raise Exception("Exceeded maximum number of switches {}".format(self.switches))
             self.switchingTimes[timeStep] = numpy.zeros(self.channelTotal, dtype = numpy.int8)
             self.switches += 1
             self.switchingTimes[timeStep][chan] = value
-    
+
     def progRepresentation(self, parse = True):
         if parse:
             self.ddsSettings = self.parseDDS()
             self.ttlProgram = self.parseTTL()
         return self.ddsSettings, self.ttlProgram
-    
+
     def userAddedDDS(self):
         return bool(len(self.ddsSettingList))
-    
+
     def parseDDS(self):
         if not self.userAddedDDS(): return None
         state = self.parent._getCurrentDDS()
-        pulses_end = {}.fromkeys(state, (0, 'stop')) #time / boolean whether in a middle of a pulse 
+        pulses_end = {}.fromkeys(state, (0, 'stop')) #time / boolean whether in a middle of a pulse
         dds_program = {}.fromkeys(state, '')
         lastTime = 0
         entries = sorted(self.ddsSettingList, key = lambda t: t[1] ) #sort by starting time
@@ -140,10 +146,10 @@ class Sequence():
         for name,num in state.iteritems():
             if not hardwareConfiguration.ddsDict[name].phase_coherent_model:
                 buf = self.parent._intToBuf(num)
-            else:  
+            else:
                 buf = self.parent._intToBuf_coherent(num)
             prog[name] += buf
-        
+
     def parseTTL(self):
         """Returns the representation of the sequence for programming the FPGA"""
         rep = ''
@@ -157,14 +163,14 @@ class Sequence():
             lastChannels = channels
         rep = rep + 2*self.numToHex(0) #adding termination
         return rep
-    
+
     def humanRepresentation(self):
         """Returns the human readable version of the sequence for FPGA for debugging"""
         dds,ttl = self.progRepresentation(parse = False)
         ttl = self.ttlHumanRepresentation(ttl)
         dds = self.ddsHumanRepresentation(dds)
         return ttl, dds
-    
+
     def ddsHumanRepresentation(self, dds):
         program = []
         print dds
@@ -186,7 +192,7 @@ class Sequence():
                     ampl_num = (256*d + c)
                     freq = freq_min +  freq_num * (freq_max - freq_min) / float(16**4 - 1)
                     ampl = ampl_min +  ampl_num * (ampl_max - ampl_min) / float(16**4 - 1)
-                    program.append((name, freq,ampl)) 
+                    program.append((name, freq,ampl))
             else:
                 for a0,a1,amp0,amp1,a2,a3,a4,a5,f0,f1,f2,f3,f4,f5,f6,f7, in chunks(arr, 16):
                     freq_num = 256**2*(256*f7 + f6) + (256*f5 + f4)
@@ -195,9 +201,9 @@ class Sequence():
                     print "freq is ", freq
                     ampl = ampl_min +  ampl_num * (ampl_max - ampl_min) / float(16**4 - 1)
                     print " ampl is ", ampl
-                    program.append((name, freq,ampl)) 
+                    program.append((name, freq,ampl))
         return program
-    
+
     def ttlHumanRepresentation(self, rep):
         rep = str(rep) ### recast rep from bytearray into string
         arr = numpy.fromstring(rep, dtype = numpy.uint16) #does the decoding from the string
@@ -213,6 +219,6 @@ class Sequence():
             expand = bin(ch)[2:].zfill(32)
             reverse = expand[::-1]
             return reverse
-        
+
         channels = map(expandChannel,channels)
         return numpy.vstack((times,channels)).transpose()
