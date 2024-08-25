283c2e949e0d599a0f37c9ad6358b2a525d06f39
thomasdellaert <dellaert.thomas@gmail.com>
2024-08-24 19:54:19 -0700

    updated the debug listener to py3, in case it's ever useful

--- lib/clients/debug_Listener.py
+++ lib/clients/debug_Listener.py
@@ -4,118 +4,105 @@ Client for debugging server signals.  Comment/uncomment signals as necessary
 """
 
 from twisted.internet.defer import inlineCallbacks
-from PyQt4 import QtGui
-
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 IMAGE_AVAILABLE_SIGNAL = 102181
-READY_4_TTL_SIGNAL     = 103810
+READY_4_TTL_SIGNAL = 103810
 
-class recieverWidget(QtGui.QWidget):
 
+class RecieverWidget(QWidget):
     debug = False
 
     # This is an ID for the client to register to the server
-    ID  = 654321
+    ID = 654321
     ID3 = 572957
-    
 
     def __init__(self, reactor, parent=None):
-        super(recieverWidget, self).__init__(parent)
-        
+        super(RecieverWidget, self).__init__(parent)
+
         self.reactor = reactor
-        
-        self.makeGUI()
-        self.connectLabrad()
 
+        self.make_gui()
+        self.connect_labrad()
 
-    def makeGUI(self):
+    def make_gui(self):
         """
         
         """
         self.setWindowTitle('Signal Listener')
 
         # Create a grid layout
-        layout = QtGui.QGridLayout()
+        layout = QGridLayout()
 
-        
-        # Create the text widget 
-        self.textedit = QtGui.QTextEdit()
+        # Create the text widget
+        self.textedit = QTextEdit()
         self.textedit.setReadOnly(True)
-        layout.addWidget(self.textedit, 1,0)
+        layout.addWidget(self.textedit, 1, 0)
 
-        
-        
-        
         self.setLayout(layout)
 
-
     @inlineCallbacks
-    def connectLabrad(self):
+    def connect_labrad(self):
         """
         Make an asynchronous connection to LabRAD
         """
         from labrad.wrappers import connectAsync
-        self.cxn = yield connectAsync(name = 'Debug Signals Client')
+        self.cxn = yield connectAsync(name='Debug Signals Client')
 
         # Connect to emitter server 
-        #self.server = self.cxn.interactive_emitter_server
+        # self.server = self.cxn.interactive_emitter_server
         self.avt_server = self.cxn.avt_indy_server
-        
-        ### Signals        
-        
-#        # Connect to signal from server (note the method is named from parsed 
-#        # text of the in the server emitter name)        
-#        yield self.server.signal__emitted_signal(self.ID)
-#        
-#        # This registers the client as a listener to the server and assigns a 
-#        # slot (function) from the client to the signal emitted from the server
-#        # In this case self.displaySignal
-#        yield self.server.addListener(listener = self.displaySignal, 
-#                source = None, ID = self.ID) 
 
-                
-                
+        ### Signals
+
+        # Connect to signal from server (note the method is named from parsed
+        # text of the in the server emitter name)
+        # yield self.server.signal__emitted_signal(self.ID)
+
+        # This registers the client as a listener to the server and assigns a
+        # slot (function) from the client to the signal emitted from the server
+        # In this case self.displaySignal
+
+        # yield self.server.addListener(listener = self.displaySignal, source = None, ID = self.ID)
+
         # Image is ready for retrieval
         yield self.avt_server.signal__image_available(IMAGE_AVAILABLE_SIGNAL)
-                                  
-        yield self.avt_server.addListener(listener=self.displaySignal, 
+
+        yield self.avt_server.addListener(listener=self.display_signal,
                                           source=self.avt_server.ID,
                                           ID=IMAGE_AVAILABLE_SIGNAL)
-
 
         # Camera is prepared for TTL input
         yield self.avt_server.signal__waiting_for_ttl(READY_4_TTL_SIGNAL)
 
-        yield self.avt_server.addListener(listener=self.displaySignal, 
+        yield self.avt_server.addListener(listener=self.display_signal,
                                           source=self.avt_server.ID,
                                           ID=READY_4_TTL_SIGNAL)
 
-
-
-    def displaySignal(self, cntx, signal):
-        
-        if self.debug : print "displaySignal called()"
+    def display_signal(self, cntx, signal):
+        if self.debug:
+            print("displaySignal called()")
         self.textedit.append(signal)
 
-
     def closeEvent(self, x):
-        
         self.cxn.disconnect()
 
         # Need to expire the listener context
-        #self.        
-        
+        # self.
+
         # Stop the reactor when closing the widget
         self.reactor.stop()
 
 
+if __name__ == "__main__":
+    # join Qt and twisted event loops
+    a = QApplication([])
+    import qt5reactor
 
-if __name__=="__main__":
-    #join Qt and twisted event loops
-    a = QtGui.QApplication( [] )
-    import qt4reactor
-    qt4reactor.install()
+    qt5reactor.install()
     from twisted.internet import reactor
-    widget = recieverWidget(reactor)
+
+    widget = RecieverWidget(reactor)
     widget.show()
     reactor.run()
